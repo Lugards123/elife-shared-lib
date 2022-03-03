ae37b6eafdb5f10f33fe57e783183040b8b38a9e
Lugards123 <89000056+Lugards123@users.noreply.github.com>
2022-03-02 19:19:24 -0500

    Update elifeApp.groovy

--- vars/elifeApp.groovy
+++ vars/elifeApp.groovy
@@ -16,7 +16,7 @@ def call(String repoUrl){
     }
    stage('Build Artifact - Maven') {
       steps {
-        sh "mvn clean package -DskipTests=false"
+        sh "mvn clean package -DskipTests=true"
         archive 'target/*.jar'
       }
    }
